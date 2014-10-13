85ad0185c0fe0a8cccf6dab548e1a01493697314
Qinsi Wang <qinsiw@andrew.cmu.edu>
2014-10-13 15:34:58 -0400

    Create evalrv.hpp

---
+++ src/evalrv.hpp
@@ -0,0 +1,40 @@
+/***********************************************************************************************
+ * Copyright (C) 2014 Qinsi Wang and Edmund M. Clarke.  All rights reserved.
+ * By using this software the USER indicates that he or she has read, understood and will comply
+ * with the following:
+ *
+ * 1. The USER is hereby granted non-exclusive permission to use, copy and/or
+ * modify this software for internal, non-commercial, research purposes only. Any
+ * distribution, including commercial sale or license, of this software, copies of
+ * the software, its associated documentation and/or modifications of either is
+ * strictly prohibited without the prior consent of the authors. Title to copyright
+ * to this software and its associated documentation shall at all times remain with
+ * the authors. Appropriated copyright notice shall be placed on all software
+ * copies, and a complete copy of this notice shall be included in all copies of
+ * the associated documentation. No right is granted to use in advertising,
+ * publicity or otherwise any trademark, service mark, or the name of the authors.
+ *
+ * 2. This software and any associated documentation is provided "as is".
+ *
+ * THE AUTHORS MAKE NO REPRESENTATIONS OR WARRANTIES, EXPRESSED OR IMPLIED,
+ * INCLUDING THOSE OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, OR THAT
+ * USE OF THE SOFTWARE, MODIFICATIONS, OR ASSOCIATED DOCUMENTATION WILL NOT
+ * INFRINGE ANY PATENTS, COPYRIGHTS, TRADEMARKS OR OTHER INTELLECTUAL PROPERTY
+ * RIGHTS OF A THIRD PARTY.
+ *
+ * The authors shall not be liable under any circumstances for any direct,
+ * indirect, special, incidental, or consequential damages with respect to any
+ * claim by USER or any third party on account of or arising from the use, or
+ * inability to use, this software or its associated documentation, even if the
+ * authors have been advised of the possibility of those damages.
+ * ***********************************************************************************************/
+#pragma once
+#include <string>
+#include <cctype>
+#include <vector>
+#include <stdlib.h>
+char cmp(char a,char b);
+double calc(double a, double b, char op);
+bool isoptr(char c);
+double eval(std::string e);
+std::vector<std::string> evalrv(std::vector<std::string> & currRVfile);
